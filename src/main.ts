ae46e9286b2cb64106348766eb9127d722d66cb3
Harvey Rogers <harveyr@gmail.com>
2019-12-16 14:32:46 -0800

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import * as core from '@actions/core'
+import * as kit from '@harveyr/github-actions-kit'
 
 async function run(): Promise<void> {
-  console.log('Hello cyber')
+  const result = await kit.execAndCapture('gcloud', [
+    'docker',
+    '--',
+    'pull',
+    'gcr.io/ua-web-ci/airship-deps:python3.7-alpine3.10-dde7594',
+  ])
+
+  console.log('result', result)
 }
 
 run().catch(err => {
