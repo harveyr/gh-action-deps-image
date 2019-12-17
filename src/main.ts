fe6be896810531a0525b92b5902ac16b527cb0fe
Harvey Rogers <harveyr@gmail.com>
2019-12-17 14:21:24 -0800

    set outputs

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,33 @@
 import * as core from '@actions/core'
-// import * as kit from '@harveyr/github-actions-kit'
+import * as kit from '@harveyr/github-actions-kit'
+
+async function pullImage(imageUrl: string): Promise<boolean> {
+  try {
+    await kit.execAndCapture('gcloud', [
+      'docker',
+      '--',
+      'pull',
+      imageUrl,
+    ])
+  } catch (err) {
+    console.log('Failed to pull image %s: %s', imageUrl, err)
+    return false
+  }
+
+  return true
+}
 
 async function run(): Promise<void> {
-  // If we ever use this in a non-gcloud scenario, we could make that an option.
   const registryUrl = core.getInput('registry_url')
   const imageName = core.getInput('image_name')
   const imageTag = core.getInput('image_tag')
 
-  console.log('registryUrl', registryUrl)
-  console.log('imageName', imageName)
-  console.log('imageTag', imageTag)
+  const imageUrl = `${registryUrl}/${imageName}:${imageTag}`
 
-  // const result = await kit.execAndCapture('gcloud', [
-  //   'docker',
-  //   '--',
-  //   'pull',
+  const pullSuccess = await pullImage(imageUrl)
 
-  //   'python3.7-alpine3.10-dde7594',
-  // ])
+  core.setOutput('image_url', imageUrl)
+  core.setOutput('image_pulled', pullSuccess.toString())
 }
 
 run().catch(err => {
