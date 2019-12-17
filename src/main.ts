10a61733fe6e77764445a14dc804cffa764aa54d
Harvey Rogers <harveyr@gmail.com>
2019-12-17 14:09:33 -0800

    log inputs

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,23 @@
 import * as core from '@actions/core'
-import * as kit from '@harveyr/github-actions-kit'
+// import * as kit from '@harveyr/github-actions-kit'
 
 async function run(): Promise<void> {
-  const result = await kit.execAndCapture('gcloud', [
-    'docker',
-    '--',
-    'pull',
-    'gcr.io/ua-web-ci/airship-deps:python3.7-alpine3.10-dde7594',
-  ])
+  // If we ever use this in a non-gcloud scenario, we could make that an option.
+  const registryUrl = core.getInput('registry_url')
+  const imageName = core.getInput('image_name')
+  const imageTag = core.getInput('image_tag')
 
-  console.log('result', result)
+  console.log('registryUrl', registryUrl)
+  console.log('imageName', imageName)
+  console.log('imageTag', imageTag)
+
+  // const result = await kit.execAndCapture('gcloud', [
+  //   'docker',
+  //   '--',
+  //   'pull',
+
+  //   'python3.7-alpine3.10-dde7594',
+  // ])
 }
 
 run().catch(err => {
