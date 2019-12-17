3b11f962c071a165b40fa980cbcf8237660ab6e3
Harvey Rogers <harveyr@gmail.com>
2019-12-16 16:03:01 -0800

    wip

--- .github/actions/publish/main.js
+++ .github/actions/publish/main.js
@@ -1,5 +1,22 @@
+import { exec } from 'child_process'
+
 async function run() {
-  console.log('hiya')
+  console.log('hiya', process.env.BRANCH)
+  // const branch
+}
+
+async function runShell(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        return reject(err)
+      }
+      if (stdout) console.log(stdout)
+      if (stderr) console.error(stdout)
+
+      return stdout + stderr
+    })
+  })
 }
 
 run().catch(err => {
